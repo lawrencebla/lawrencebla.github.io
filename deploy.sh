cfbdb9a6f6883ede0deaceccff9673114ae6f25e
Lawrence <liuliang3@kingsoft.com>
2019-09-16 15:43:00 +0800

    'test'

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
 set -e
 
-if [[ -z "$GITHUB_TOKEN" ]]; then
-    echo "Set the GITHUB_TOKEN env variable."
-    exit 1
-fi
-
 remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 echo ${remote_repo}
@@ -14,7 +9,8 @@ git config --global user.name "github action"
 
 mkdir .deploy
 cd .deploy
-echo git clone --depth 1 --branch master --single-branch ${remote_repo} . || (git init && git remote add -t master origin ${remote_repo})
+
+git clone --depth 1 --branch master --single-branch ${remote_repo} . || (git init && git remote add -t master origin ${remote_repo})
 
 rm -rf ./* 
 cp -r ../public/* .
